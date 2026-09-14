9a7b67a90f284123d3b734a4f8d2954600d245fd
Adaamss <aademhamdi@gmail.com>
2026-09-14 19:58:44 +0100

    feat: add MongoDB connection config

---
+++ src/config/dbConfig.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export async function connectDB(): Promise<void> {
+  const uri = process.env.MONGO_URI as string;
+
+  try {
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 5000,
+    });
+    console.log("Connected to local MongoDB successfully");
+  } catch (err) {
+    console.error("Connection error:", err);
+    throw err;
+  }
+}
